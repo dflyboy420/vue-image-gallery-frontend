a121dbb8d3435a72527f921231dcf5a2eba57414
dflyboy420 <daniel@dpulm.online>
2021-07-21 22:05:25 +0200

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import Vue from "vue";
 import Vuex from "vuex";
 
@@ -15,12 +16,23 @@ export default new Vuex.Store({
       state.images.uris.push(img.uri);
       state.images.files.push(img.file);
     },
+    clear(state) {
+      state.images = {
+        uris: [],
+        files: [],
+      };
+    }
   },
   actions: {},
   modules: {},
   getters: {
     imgNum(state) {
       return state.images.uris.length;
+    },
+    formData(state) {
+      const fd = new FormData();
+      _.each(state.images.files, (f) => fd.append("img", f));
+      return fd;
     },
   },
 });
